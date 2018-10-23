796292e8324e165d84b562b991a665a81dc180ea
Picnic <yaowarets58@email.nu.ac.th>
2018-10-23 20:17:13 +0700

    form validation

--- js/analysis.js
+++ js/analysis.js
@@ -1,27 +1,22 @@
+(function() {
+  'use strict';
+  window.addEventListener('load', function() {
+    // Fetch all the forms we want to apply custom Bootstrap validation styles to
+    var forms = document.getElementsByClassName('needs-validation');
+    // Loop over them and prevent submission
+    var validation = Array.prototype.filter.call(forms, function(form) {
+      form.addEventListener('submit', function(event) {
+        if (form.checkValidity() === false) {
+          event.preventDefault();
+          event.stopPropagation();
+        }
+        form.classList.add('was-validated');
+      }, false);
+    });
+  }, false);
+})();
+
 $(document).ready(function(){
-
-  // $("p").click(function(){
-  //     $(this).hide();
-  // });
-
-
-  (function() {
-    'use strict';
-    window.addEventListener('load', function() {
-      // Fetch all the forms we want to apply custom Bootstrap validation styles to
-      var forms = document.getElementsByClassName('needs-validation');
-      // Loop over them and prevent submission
-      var validation = Array.prototype.filter.call(forms, function(form) {
-        form.addEventListener('submit', function(event) {
-          if (form.checkValidity() === false) {
-            event.preventDefault();
-            event.stopPropagation();
-          }
-          form.classList.add('was-validated');
-        }, false);
-      });
-    }, false);
-  })();
 
   $('#submitForm').on('click', function(){
     var year = $('#year').val();
@@ -49,6 +44,7 @@ $(document).ready(function(){
     var pain = $('#pain').val();
     var urinary_dis = $('#urinary_dis').val();
     var dyspnea = $('#dyspnea').val();
+
 
     var result;
 
